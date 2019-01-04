ea40d4bf5ff6d5e50381813557611dc5454c2fd3
lifu <zzcm@zzcmdeimac-2.local>
2019-01-04 16:39:29 +0800

    Initial Commit

---
+++ LeafScratchableLatex/LeafScratchableLatex.h
@@ -0,0 +1,19 @@
+//
+//  LeafScratchableLatex.h
+//  LeafScratchableLatex
+//
+//  Created by ZZCM on 2019/1/4.
+//  Copyright © 2019 ZZCM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LeafScratchableLatex.
+FOUNDATION_EXPORT double LeafScratchableLatexVersionNumber;
+
+//! Project version string for LeafScratchableLatex.
+FOUNDATION_EXPORT const unsigned char LeafScratchableLatexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeafScratchableLatex/PublicHeader.h>
+
+
